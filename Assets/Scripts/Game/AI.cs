d06f3cf9eee2b33311cc878ab69bd68e5345cbb7
Quark <gennadiy.kaydalov@mail.ru>
2022-06-29 15:49:32 +0300

    AI
    Added:
    *Made AI based on waypoints system

--- Assets/Scripts/Game/AI.cs
+++ Assets/Scripts/Game/AI.cs
@@ -2,17 +2,123 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public struct DestinationDots
+{
+    public Transform From;
+    public List<Transform> To;
+}
+
 public class AI : MonoBehaviour
 {
+    public List<DestinationDots> Ways;
+    public float MovingDelay;
+
+    [Range(0,50)]
+    public float MovingSpeed;
+
+    private float MovingDelayTimer;
+
+    private Vector3 FromPoint;
+    private Vector3 ToPoint;
+
+    private int IndexCurrentPoint;
+
+    [HideInInspector]
+    public SpawnManager spawnManger;
+
+    [HideInInspector]
+    public int IndexTypesOfEnemy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        FromPoint = transform.position;
+        FindingIndexOfSpawnPointInGeneralList();
+
+        MovingDelayTimer = MovingDelay;
+    }
+
+    bool FindingIndexOfSpawnPointInGeneralList()
+    {
+        for (int i = 0; i < spawnManger.PosForSpawn[IndexTypesOfEnemy].Count; i++)
+        {
+            if (transform.position == spawnManger.PosForSpawn[IndexTypesOfEnemy][i])
+            {
+                IndexCurrentPoint = i;
+                return true;
+            }
+        }
+        return false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        CheckPosition();
+        Moving();
+    }
+
+    bool CheckPosition()
+    {
+        //timer
+        if (MovingDelayTimer > 0)
+        {
+            MovingDelayTimer -= Time.deltaTime;
+        }
+        else
+        {
+            int randDest = Random.Range(0, Ways[IndexCurrentPoint].To.Count);
+            for (int i = 0; i < spawnManger.AvailablePosForSpawn[IndexTypesOfEnemy].Count; i++)
+            {
+                //check available destination
+                if (Ways[IndexCurrentPoint].To[randDest].position == spawnManger.AvailablePosForSpawn[IndexTypesOfEnemy][i])
+                {
+                    //Preparing to moving
+                    FromPoint = transform.position;
+                    ToPoint = Ways[IndexCurrentPoint].To[randDest].position;
+                    spawnManger.AvailablePosForSpawn[IndexTypesOfEnemy].RemoveAt(i);
+                    IsMoving = true;
+
+                    //Restart timer
+                    MovingDelayTimer = MovingDelay;
+
+                    return true;
+                }
+            }
+
+            //Restart timer
+            MovingDelayTimer = MovingDelay;
+        }
+        return false;
+    }
+
+    private bool IsMoving;
+    void Moving()
+    {
+        if (IsMoving)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(transform.position, ToPoint, MovingSpeed * Time.deltaTime);
+            if (gameObject.transform.position == ToPoint)
+            {
+                FindingIndexOfSpawnPointInGeneralList();
+                spawnManger.AvailablePosForSpawn[IndexTypesOfEnemy].Add(FromPoint);
+                IsMoving = false;
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        for (byte i = 0; i < Ways.Count; i++)
+        {
+            for (byte k = 0; k < Ways[i].To.Count; k++)
+            {
+                // Draws a blue line from this transform to the target
+                Gizmos.color = Color.blue;
+                Gizmos.DrawLine(Ways[i].From.position, Ways[i].To[k].position);
+            }
+        }
     }
 }
--- Assets/Scripts/Game/SpawnManager.cs
+++ Assets/Scripts/Game/SpawnManager.cs
@@ -12,29 +12,30 @@ public class SpawnManager : MonoBehaviour
     private float TimeRemaining;
 
     public List<List<Vector3>> PosForSpawn;
-    public List<List<Vector3>> FreePosForSpawn;
+    public List<List<Vector3>> AvailablePosForSpawn;
 
     [SerializeField]
     private List<GameObject> StorageOfPos;
 
-    public List<GameObject> Enemy;
+    public List<GameObject> TypesOfEnemy;
 
     void Start()
     {
         PosForSpawn = new List<List<Vector3>>();
-        FreePosForSpawn = new List<List<Vector3>>();
+        AvailablePosForSpawn = new List<List<Vector3>>();
 
         //Initialize list with pos for spawn
         for (byte i = 0; i < StorageOfPos.Count; i++)
         {
             PosForSpawn.Add(new List<Vector3>());
+            AvailablePosForSpawn.Add(new List<Vector3>());
 
             for (byte k = 0; k < StorageOfPos[i].transform.childCount; k++)
             {
-                PosForSpawn[i].Add(StorageOfPos[i].transform.GetChild(i).transform.position);
+                PosForSpawn[i].Add(StorageOfPos[i].transform.GetChild(k).transform.position);
+                AvailablePosForSpawn[i].Add(StorageOfPos[i].transform.GetChild(k).transform.position);
             }
         }
-        FreePosForSpawn = PosForSpawn;
 
         //Initialize timer
         TimeRemaining = SpawnDelay;
@@ -55,13 +56,21 @@ public class SpawnManager : MonoBehaviour
         else
         {
             #region Spawn Enemy
-            int randEnemy = Random.Range(0, Enemy.Count);
-            int randPos = Random.Range(0, FreePosForSpawn[randEnemy].Count);
-            Instantiate(Enemy[randEnemy], FreePosForSpawn[randEnemy][randPos], Quaternion.identity);
-            #endregion
+            int randEnemy = Random.Range(0, TypesOfEnemy.Count);
+            int randPos = Random.Range(0, AvailablePosForSpawn[randEnemy].Count);
 
-            //Deleting point from list of available for spawning
-            FreePosForSpawn[randEnemy].RemoveAt(randPos);
+            if (AvailablePosForSpawn[randEnemy].Count != 0)
+            {
+                Transform obj = Instantiate(TypesOfEnemy[randEnemy], AvailablePosForSpawn[randEnemy][randPos], Quaternion.identity).transform;
+
+                obj.GetComponent<AI>().IndexTypesOfEnemy = randEnemy;
+                obj.GetComponent<AI>().spawnManger = this;
+                obj.GetComponent<AI>().enabled = true;
+
+                //Deleting point from list of available for spawning
+                AvailablePosForSpawn[randEnemy].RemoveAt(randPos);
+            }
+            #endregion
 
             //Restart timer
             TimeRemaining = SpawnDelay;
