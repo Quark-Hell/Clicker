48fdd2827f4d01ab9d97dadb9d876604316bbb2d
Quark <gennadiy.kaydalov@mail.ru>
2022-06-29 20:32:03 +0300

    Menu
    Added:
    *Remade menu for landscape orientation
    *Made smooth switching between types of menu(now work only with main menu and credits because other types of menu doesn't exist)

--- Assets/Scripts/Menu/ButtonManager.cs
+++ Assets/Scripts/Menu/ButtonManager.cs
@@ -4,28 +4,163 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
+[System.Serializable]
+public class TypeOfMenu {
+    [SerializeField]
+    private string Name;    //just for developer
+
+    public List<GameObject> Object;
+    public Vector3 Displacement;
+    [Range(0, 1)] public float SpeedOfShift;
+    public bool IsShift;
+    public bool Show;
+
+    [HideInInspector]
+    public List<Vector3> ObjectStartPos;
+}
+
 public class ButtonManager : MonoBehaviour
 {
-    [SerializeField] private string NewGame = "Game";
-    // Start is called before the first frame update
+    [SerializeField] private string Level = "Game";
 
-    public void NewGameButton()
+    public List<TypeOfMenu> MainMenu;
+
+    private void Start()
     {
-        SceneManager.LoadScene(NewGame);
+        //Set start point
+        for (int i = 0; i < MainMenu.Count; i++)
+        {
+            for (int k = 0; k < MainMenu[i].Object.Count; k++)
+            {
+                MainMenu[i].ObjectStartPos.Add(MainMenu[i].Object[k].transform.position);
+            }
+        }
+
+        StartMovingPartsOfMenu();
+        EnabledPartsOfMenu(true);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(Level);
     }
 
     public void Stats()
     {
 
     }
-
-    public void Credits()
+    public void BackFromStats()
     {
 
     }
 
+    public void Credits()
+    {
+        //Main menu
+        MainMenu[0].IsShift = true;
+        MainMenu[0].Show = false;
+
+        //Credits
+        MainMenu[2].IsShift = true;
+        MainMenu[2].Show = true;
+    }
+    public void BackFromCredits()
+    {
+        //Main menu
+        MainMenu[2].IsShift = true;
+        MainMenu[2].Show = false;
+
+        //Credits
+        MainMenu[0].IsShift = true;
+        MainMenu[0].Show = true;
+    }
+
     public void Exit()
     {
+        Application.Quit();
+    }
 
+    void MovingPartsOfMenu()
+    {
+        //Check all types of menu for needed to moving
+        for (int i = 0; i < MainMenu.Count; i++)
+        {
+            if (MainMenu[i].IsShift)
+            {
+                //Moving all objects in type of menu
+                for (int k = 0; k < MainMenu[i].Object.Count; k++)
+                {
+                    //Showing
+                    if (MainMenu[i].Show)
+                    {
+                        MainMenu[i].Object[k].transform.position = Vector3.Lerp(MainMenu[i].Object[k].transform.position, MainMenu[i].ObjectStartPos[k], MainMenu[i].SpeedOfShift);
+                        if (Vector2.Distance(MainMenu[i].Object[k].transform.position, MainMenu[i].ObjectStartPos[k]) < 0.01f)
+                        {
+                            MainMenu[i].IsShift = false;
+                            MainMenu[i].Show = true;
+                        }
+                    }
+                    //Hidding
+                    else
+                    {
+                        MainMenu[i].Object[k].transform.position = Vector3.Lerp(MainMenu[i].Object[k].transform.position, MainMenu[i].ObjectStartPos[k] + MainMenu[i].Displacement, MainMenu[i].SpeedOfShift);
+                        if (Vector2.Distance(MainMenu[i].Object[k].transform.position, MainMenu[i].ObjectStartPos[k] + MainMenu[i].Displacement) < 0.01f)
+                        {
+                            MainMenu[i].IsShift = false;
+                            MainMenu[i].Show = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //Used only in start
+    void StartMovingPartsOfMenu()
+    {
+        //Check all types of menu for needed to moving
+        for (int i = 0; i < MainMenu.Count; i++)
+        {
+            if (MainMenu[i].IsShift)
+            {
+                //Moving all objects in type of menu
+                for (int k = 0; k < MainMenu[i].Object.Count; k++)
+                {
+                    //Showing
+                    if (MainMenu[i].Show)
+                    {
+                        MainMenu[i].Object[k].transform.position = MainMenu[i].ObjectStartPos[k];
+
+                        MainMenu[i].IsShift = false;
+                        MainMenu[i].Show = true;
+                    }
+                    //Hidding
+                    else
+                    {
+                        MainMenu[i].Object[k].transform.position = MainMenu[i].ObjectStartPos[k] + MainMenu[i].Displacement;
+
+                        MainMenu[i].IsShift = false;
+                        MainMenu[i].Show = false;
+                    }
+                }
+            }
+        }
+    }
+
+    //Used only in start
+    void EnabledPartsOfMenu(bool IsEnabled)
+    {
+        for (int i = 0; i < MainMenu.Count; i++)
+        {
+            for (int k = 0; k < MainMenu[i].Object.Count; k++)
+            {
+                MainMenu[i].Object[k].SetActive(true);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        MovingPartsOfMenu();
     }
 }
